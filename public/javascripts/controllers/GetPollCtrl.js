6271ad535a46d37c9881f5ea4d96675503759168
Ethernetz <Ethannetz@hotmail.com>
2017-06-06 16:01:06 -0700

    GetController Editing

--- public/javascripts/controllers/GetPollCtrl.js
+++ public/javascripts/controllers/GetPollCtrl.js
@@ -4,26 +4,17 @@ function GetPollCtrl($scope, $http, $routeParams, $q, $rootScope) {
     $scope.moduleState = "notVoted";
 
     $scope.loadPage = function () {
-        var id = $routeParams.id;
-        id = id.substring(3, (id.length - 3));
-        $http.get('/api/getPoll/' + id).success(function (singlePoll) {
-            $scope.poll = singlePoll;
-            $scope.calculatePercentage();
-            $scope.poll.options[0].bgurl = "url('http://www.conradnewyork.com/assets/img/Special-Offers/new-york-skyline-720x565.jpg')";
-            $scope.poll.options[1].bgurl = "url('http://2.bp.blogspot.com/-D3UB8Nz30zM/USv8WYmvSdI/AAAAAAAAB5o/S8rA7KrQYdM/s1600/seattle+skyline.jpg')";
-        });
-        if ($rootScope.pollsVotedIn.includes(id)) {
-            $scope.moduleState = "voted";
-        } else {
-            $scope.moduleState = "notVoted";
-        }
-
+        var id = ($routeParams.id).substring(3, ($routeParams.id.length - 3));
+        retrievePoll(id);
+        $rootScope.pollsVotedIn.includes(id) ? $scope.moduleState = "voted" : $scope.moduleState = "notVoted";
     };
+
     $scope.submitVote = function (text, id) {
         var payload = {
             poll_id: id,
             choice_text: text
         };
+
         var defer = $q.defer();
 
         defer.promise.then(function () {
@@ -37,24 +28,19 @@ function GetPollCtrl($scope, $http, $routeParams, $q, $rootScope) {
         });
 
         defer.resolve();
-
-
     }
-    $scope.showPoll = function () {
-        console.log($scope.poll);
-    };
 
     $scope.calculatePercentage = function () {
         var sum = 0;
         for (var i = 0; i < $scope.poll.options.length; i++) {
-            sum = sum + $scope.poll.options[i].votes;
-            $scope.poll.options[i].color = "black";
+            sum += $scope.poll.options[i].votes;
         }
+
         if (sum > 0) {
             for (var i = 0; i < $scope.poll.options.length; i++) {
                 $scope.poll.options[i].percentage = Math.round(($scope.poll.options[i].votes / sum) * 100);
                 $scope.poll.options[i].height = $scope.poll.options[i].percentage * 3;
-            }
+              }
         } else {
             for (var i = 0; i < $scope.poll.options.length; i++) {
                 $scope.poll.options[i].percentage = 0;
@@ -62,13 +48,19 @@ function GetPollCtrl($scope, $http, $routeParams, $q, $rootScope) {
         }
 
         if ($scope.poll.options.length = 2) {
-            if ($scope.poll.options[0].percentage > $scope.poll.options[1].percentage) {
-                $scope.poll.options[0].color = "#ff5765";
-                $scope.poll.options[1].color = "black";
-            } else if ($scope.poll.options[0].percentage < $scope.poll.options[1].percentage) {
-                $scope.poll.options[0].color = "black";
-                $scope.poll.options[1].color = "#ff5765";
+            $scope.poll.options[0].color = "#000"
+            $scope.poll.options[1].color = "#000"
+            if($scope.poll.options[0].percentage > $scope.poll.options[1].percentage) ($scope.poll.options[0].color = "#ff5765");
+            if ($scope.poll.options[0].percentage < $scope.poll.options[1].percentage) ($scope.poll.options[1].color = "#ff5765");
             }
-        }
-    };
+        };
+
+    function retrievePoll(id){
+        $http.get('/api/getPoll/' + id).success(function (singlePoll) {
+            $scope.poll = singlePoll;
+            $scope.calculatePercentage();
+            $scope.poll.options[0].bgurl = "url('http://www.conradnewyork.com/assets/img/Special-Offers/new-york-skyline-720x565.jpg')";
+            $scope.poll.options[1].bgurl = "url('http://2.bp.blogspot.com/-D3UB8Nz30zM/USv8WYmvSdI/AAAAAAAAB5o/S8rA7KrQYdM/s1600/seattle+skyline.jpg')";
+        });
+    }
   };
