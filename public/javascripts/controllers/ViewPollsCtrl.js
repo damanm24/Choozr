96c2939f528510e3a73a54ed9e9fb851023bb2af
damanm24 <dmulye@eastsideprep.org>
2017-06-06 17:17:00 -0700

    Calculated the E-Field

--- public/javascripts/controllers/ViewPollsCtrl.js
+++ public/javascripts/controllers/ViewPollsCtrl.js
@@ -1,5 +1,7 @@
 function ViewPollsCtrl($scope, $http) {
   $scope.loadPage = function() {
-    $http.get('')
-  }
+    $http.get('/api/getPolls/').success(function (allPolls) {
+      console.log(allPolls);
+    });
+  };
 }
