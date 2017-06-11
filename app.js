82d3c2325a82f29d3facf541d2e1ce23657581b9
damanm24 <dmulye@eastsideprep.org>
2017-06-10 19:12:43 -0700

    Restarted ViewPolls

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -40,22 +42,22 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.sendFile(path.join(__dirname, 'views') + '/index.html');
+	// render the error page
+	res.status(err.status || 500);
+	res.sendFile(path.join(__dirname, 'views') + '/index.html');
 });
 
-server.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/controllers/ViewPollsCtrl.js
+++ public/javascripts/controllers/ViewPollsCtrl.js
@@ -1,58 +1,23 @@
 function ViewPollsCtrl($q, $scope, $http) {
 
-  $scope.polls =
-        	[
-            	{
-                	"name":	"module1",
-        			"moduleState": "notVoted"
-                },
-                {
-                	"name":	"module2",
-        			"moduleState": "voted"
-                }
-            ];
+	let loadPage = function() {
+		return new Promise((resolve, reject) => {
+			$http.get('/api/getPolls/')
+				.then(function(response) {
+					$scope.polls = response.data;
+					resolve(response.data);
+				})
+				.catch(function(err, status) {
+					reject(err);
+				})
+		})
+	};
 
-
-
-
-  $scope.addStates = function() {
-    for (var i = 0; i < $scope.polls.length; i++) {
-      $scope.polls[i].moduleState = "notVoted";
-      console.log($scope.polls[i]);
-    }
-  }
-
-  $scope.loadSinglePoll = function(id, currentPollIndex) {
-    var id = ($routeParams.id).substring(3, ($routeParams.id.length - 3));
-    getSinglePoll(id);
-    $rootScope.pollsVotedIn.includes(id) ? $scope.polls[currentPollIndex].moduleState = "voted" : $scope.polls[currentPollIndex].moduleState = "notVoted";
-  }
-
-
-$scope.loadPage = function () {
-  var defer = $q.defer();
-  defer.promise
-    .then(function() {
-      $http.get('/api/getpolls/').success(function(allPolls) {
-        console.log(allPolls);
-        $scope.polls = allPolls;
-        $scope.addStates();
-      });
-    })
-
-    defer.resolve();
-};
-
-
-
-  function getSinglePoll(id, currentPollIndex) {
-    $http.get('/api/getPoll/' + id).success(function(singlePoll) {
-      $scope.polls[currentPollIndex] = singlePoll;
-      console.log(singlePoll);
-      $scope.calculatePercentage();
-      $scope.polls[currentPollIndex].options[0].bgurl = "url('http://www.conradnewyork.com/assets/img/Special-Offers/new-york-skyline-720x565.jpg')";
-      $scope.polls[currentPollIndex].options[1].bgurl = "url('http://2.bp.blogspot.com/-D3UB8Nz30zM/USv8WYmvSdI/AAAAAAAAB5o/S8rA7KrQYdM/s1600/seattle+skyline.jpg')";
-    });
-  }
+	loadPage()
+		.then(function(data) {
+			for (var i = 0; i < $scope.polls.length; i++) {
+				$scope.polls[i].hasVoted = false;
+			}
+		});
 
 }
