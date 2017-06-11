f8acd1c2d3af7bfa93c514c1b45f354ae9d985f9
damanm24 <dmulye@eastsideprep.org>
2017-06-10 20:33:49 -0700

    Commented ViewPolls Ctrl
    Existing bug on pageload for viewPolls

--- public/javascripts/controllers/GetPollCtrl.js
+++ public/javascripts/controllers/GetPollCtrl.js
@@ -1,80 +1,78 @@
 function GetPollCtrl($scope, $http, $routeParams, $q, $rootScope) {
 
 
-  $scope.poll = null;
+	$scope.poll = null;
 
-  $scope.loadPage = function() {
-    var id = $routeParams.id.replace(/\"/g, "");
-    retrievePoll(id);
-    $rootScope.pollsVotedIn.includes(id) ? $scope.vState = "voted" : $scope.vState = "not-voted";
-  };
+	$scope.loadPage = function() {
+		var id = $routeParams.id.replace(/\"/g, "");
+		retrievePoll(id);
+		$rootScope.pollsVotedIn.includes(id) ? $scope.vState = "voted" : $scope.vState = "not-voted";
+	};
 
-  $scope.submitVote = function(text, id) {
-    var payload = {
-      poll_id: id,
-      choice_text: text
-    };
+	$scope.submitVote = function(text, id) {
+		var payload = {
+			poll_id: id,
+			choice_text: text
+		};
 
-    var defer = $q.defer();
+		var defer = $q.defer();
 
-    defer.promise.then(function() {
-      $http.put("/api/vote/", payload);
-    }).then(function() {
-      $scope.vState = "voted";
-    }).then(function() {
-      $rootScope.pollsVotedIn.push(id);
-    }).then(function() {
-      $scope.loadPage();
-    });
+		defer.promise.then(function() {
+				$http.put("/api/vote/", payload);
+			})
+			.then(function() {
+				$scope.vState = "voted";
+			})
+			.then(function() {
+				$rootScope.pollsVotedIn.push(id);
+			})
+			.then(function() {
+				$scope.loadPage();
+			});
 
-    defer.resolve();
-  };
+		defer.resolve();
+	};
 
-  $scope.calculatePercentage = function() {
-    var sum = 0;
-    for (var i = 0; i < $scope.poll.options.length; i++) {
-      sum += $scope.poll.options[i].votes;
-    }
-    if (sum > 0) {
-      for (var i = 0; i < $scope.poll.options.length; i++) {
-        $scope.poll.options[i].percentage = Math.round(($scope.poll.options[i].votes / sum) * 100);
-        $scope.poll.options[i].height = $scope.poll.options[i].percentage * 3;
-        $scope.poll.options[i].color = "black";
-      }
-    } else {
-      for (var i = 0; i < $scope.poll.options.length; i++) {
-        $scope.poll.options[i].percentage = 0;
-      }
-    }
-    var maxValue = $scope.poll.options[0].percentage;
-    var maxIndex = 0;
-    var maxCount = 0;
-    for (var i = 1; i < $scope.poll.options.length; i++) {
-      if ($scope.poll.options[i].percentage > maxValue) {
-        maxValue = $scope.poll.options[i].percentage;
-        maxIndex = i;
-      }
-    }
+	$scope.calculatePercentage = function() {
+		var sum = 0;
+		$scope.poll.options.forEach(function(element) {
+			sum += element
+		})
+		for (var i = 0; i < $scope.poll.options.length; i++) {
+			$scope.poll.options[i].percentage = Math.round(($scope.poll.options[i].votes / sum) * 100);
+			$scope.poll.options[i].height = $scope.poll.options[i].percentage * 3;
+			$scope.poll.options[i].color = "black";
+		}
+		var maxValue = $scope.poll.options[0].percentage;
+		var maxIndex = 0;
+		var maxCount = 0;
+		for (var i = 1; i < $scope.poll.options.length; i++) {
+			if ($scope.poll.options[i].percentage > maxValue) {
+				maxValue = $scope.poll.options[i].percentage;
+				maxIndex = i;
+			}
+		}
 
-    for (var i = 0; i < $scope.poll.options.length; i++) {
-      if ($scope.poll.options[i].percentage == maxValue) {
-        maxCount++;
-      }
-    }
+		for (var i = 0; i < $scope.poll.options.length; i++) {
+			if ($scope.poll.options[i].percentage == maxValue) {
+				maxCount++;
+			}
+		}
 
-    if (maxCount == 1) {
-      $scope.poll.options[maxIndex].color = "#ff5765";
-    }
-  };
+		if (maxCount == 1) {
+			$scope.poll.options[maxIndex].color = "#ff5765";
+		}
+	};
 
-  function retrievePoll(id) {
-    $http.get('/api/getPoll/' + id).then(function(singlePoll) {
-      $scope.poll = singlePoll.data;
-      $scope.calculatePercentage();
-      for (var i = 0; i < $scope.poll.options.length; i++) {
-        $scope.poll.options[i].bgurl = "url('" + $scope.poll.options[i].imageURL + "')";
-        $scope.poll.options[i].width = 800 / ($scope.poll.options.length);
-      }
-    });
-  }
+	function retrievePoll(id) {
+		$http.get('/api/getPoll/' + id)
+			.then(function(singlePoll) {
+				$scope.poll = singlePoll.data;
+				$scope.calculatePercentage();
+				for (var i = 0; i < $scope.poll.options.length; i++) {
+					$scope.poll.options[i].bgurl = "url('" + $scope.poll.options[i].imageURL + "')";
+					$scope.poll.options[i].width = 800 / ($scope.poll.options.length);
+				}
+			});
+	}
 };
--- public/javascripts/controllers/ViewPollsCtrl.js
+++ public/javascripts/controllers/ViewPollsCtrl.js
@@ -1,5 +1,5 @@
 function ViewPollsCtrl($q, $scope, $http) {
-
+	//This is the load page promise it sets the $scope.polls to the response it gets back
 	let loadPage = function() {
 		return new Promise((resolve, reject) => {
 			$http.get('/api/getPolls/')
@@ -14,10 +14,14 @@ function ViewPollsCtrl($q, $scope, $http) {
 	};
 
 
-	loadPage()
+	loadPage() //LoadPage gets called when the page loads.
 		.then(function(data) {
-			for (var i = 0; i < $scope.polls.length; i++) {
-				$scope.polls[i].hasVoted = false;
+			for (var i = 0; i < $scope.polls.length; i++) { //The outer for-loop adds a hasVoted property which is set to false by default
+				$scope.polls[i].hasVoted = "false";
+				for (var j = 0; j < $scope.polls[i].options.length; j++) { //This for loop was copied from the GetPollCtrl and it gets the bgurl and the width
+					$scope.polls[i].options[j].bgurl = "url('" + $scope.polls[i].options[j].imageURL + "')"; //NOTE its a nested forloop so it has to be polls[i] and then options[j]
+					$scope.polls[i].options[j].width = 800 / ($scope.polls[i].options.length);
+				}
 			}
 		});
 
@@ -25,10 +29,13 @@ function ViewPollsCtrl($q, $scope, $http) {
 		return new Promise((resolve, reject) => {
 				$http.put("/api/vote/", payload);
 			})
-			.then(function(response) {
-				response.data.hasVoted = true;
-				$scope.polls[currentPollIndex] = response.data;
+			.then(function(response) { //NOTE we've set up the /api/vote/ route to return the latest version of the poll by default, so NOTE we don't need another function to do that
+				response.data.hasVoted = "true"; //Adds the property hasVoted, but set to true
+				$scope.polls[currentPollIndex] = response.data; //then sets the index of that poll to the updated poll
 				resolve(response.data);
+			})
+			.catch(function(err) {
+				reject(err);
 			});
 	}
 
@@ -37,9 +44,9 @@ function ViewPollsCtrl($q, $scope, $http) {
 			choice_text: optionText,
 			poll_id: poll_id
 		}
-		sendVoteRequest(payload, currentPollIndex)
+		sendVoteRequest(payload, currentPollIndex) //NOTE I haven't done anything with changing the view of a poll that has been voted on just an FYI
 			.then(function(singlePoll) {
-				//calculatePercentage should go here
+				//NOTE calculatePercentage should go here
 			});
 
 	}
