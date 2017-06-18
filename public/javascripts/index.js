5603786bda1b0c30080bfe1434cba96c68f13a53
damanm24 <dmulye@eastsideprep.org>
2017-06-18 11:45:12 -0700

    Switched from ng-route to ui.router

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,20 @@
 /**
  * Created by daman on 5/31/2017.
  */
-var app = angular.module('Choozr', ["ngRoute", "ngCookies"]);
+var app = angular.module('Choozr', ["ngCookies", "ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/partials/home.html'
+    })
+    .state('myPolls', {
+      url: '/myPolls',
+      templateUrl: '/partials/myPolls.html'
+    });
+
+});
